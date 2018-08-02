6b4661b033628cd13f02eac37e4bfa1fbeea125c
Jonas Rodon <jonasrodon@gmail.com>
2018-08-02 18:01:37 +0200

    primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,19 @@
+var socket = io()
+// Para escuchar
+socket.on('connect', function () {
+  console.log('Conectado al servidor')
+})
+socket.on('disconnect', function () {
+  console.log('Perdimos conexión con el servidor')
+})
+// Enviar información
+socket.emit('enviarMensaje', {
+  usuario: 'Jonas',
+  mensaje: 'Hola Mundo'
+}, function (resp) {
+  console.log('respuesta server: ', resp)
+})
+// Escuchar información
+socket.on('enviarMensaje', function (mensaje) {
+  console.log('Servidor:', mensaje)
+})
